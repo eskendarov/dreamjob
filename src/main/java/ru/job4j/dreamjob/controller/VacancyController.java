13f9db49744cf2bbe3ef1daf2b6663ca8704a220
Enver Eskendarov <envereskendarov@gmail.com>
2023-12-29 01:40:20 -0800

    8. HTML form. Страница создания вакансии [#504844]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -18,4 +18,10 @@ public class VacancyController {
         model.addAttribute("vacancies", vacancyRepository.findAll());
         return "vacancies/list";
     }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "vacancies/create";
+    }
+
 }
