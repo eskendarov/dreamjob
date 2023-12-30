fe9ebd07c7c5104d0d2d48b28a541ecd6911a07d
Enver Eskendarov <envereskendarov@gmail.com>
2023-12-29 17:23:07 -0800

    fix: 5. Просмотр, редактирование и удаление вакансии [#504850 #469553]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -37,7 +37,7 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(),
-                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate())) != null;
+                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle(), vacancy.getDescription(), oldVacancy.getCreationDate())) != null;
     }
 
     @Override
